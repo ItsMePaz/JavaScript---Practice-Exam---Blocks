122815da1bc0075e7830a06e1b746c9a7fd24178
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-07-12 13:29:20 +0800

    Finished exam

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 console.log("Hey");
-let blocks = [2, 6, 8, 2];
 
-let frogA = 1;
-let frogB = 1;
+function solution(blocks, startingBlock) {
+  let frogA = 1;
+  let frogB = 0;
 
-while (blocks.length > 0) {
-  if (blocks[0] <= blocks[1]) {
-    ++frogA;
-    console.log("can jump");
-    blocks.shift();
-  } else {
-    blocks.length = 0;
-    frogA - 1;
+  let frogAArray = blocks.slice(startingBlock);
+  console.log(frogAArray);
+  let frogArraySliced = blocks.reverse();
+
+  let frogBArray = frogArraySliced.slice(startingBlock - 2);
+
+  console.log(frogBArray);
+  while (frogAArray.length > 0) {
+    if (frogAArray[0] <= frogAArray[1]) {
+      ++frogA;
+
+      frogAArray.shift();
+    } else {
+      frogAArray.length = 0;
+    }
   }
+
+  while (frogBArray.length > 0) {
+    if (frogBArray[0] <= frogBArray[1]) {
+      ++frogB;
+
+      frogBArray.shift();
+    } else {
+      frogBArray.length = 0;
+    }
+  }
+  console.log(
+    `The longest possible distance between the two frogs is ${frogA + frogB}`
+  );
 }
 
-console.log(frogA);
-
-let random = [2, 6, 8, 2];
-console.log(random);
-let newArray = random.splice(2);
-console.log(newArray);
+solution([2, 6, 8, 5], 0);
