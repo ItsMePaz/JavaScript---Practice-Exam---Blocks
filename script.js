805f42d5df86a21070248d4810c0991eeada10bb
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-07-12 16:19:31 +0800

    Finished with DOM Manipulation

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 console.log("Hey");
 
+const show = document.getElementById("show");
+const hide = document.getElementById("hide");
+const example = document.getElementById("example");
+
+const arrayBlockInput = document.getElementById("arrayBlock");
+const displayInput = document.getElementById("displayInput");
+const submit = document.getElementById("submit");
+
+const startingBlockInput = document.getElementById("startingBlock");
+const displayStaringIndexInput = document.getElementById("startingIndexInput");
+const submitIndex = document.getElementById("submitIndex");
+
+const longestDistanceApart = document.getElementById("longestDistance");
+let blocks = [];
+let indexStart = "";
+
+show.addEventListener("click", showExamples);
+
+function showExamples() {
+  example.style.display = "";
+}
+
+hide.addEventListener("click", hideExamples);
+
+function hideExamples() {
+  example.style.display = "none";
+}
+
+function submitInput(e) {
+  e.preventDefault();
+  /* displayInput.innerText = `[${arrayBlockInput.value}]`; */
+
+  blocks.push(parseInt(arrayBlockInput.value));
+  console.log(blocks);
+  displayInput.innerText = `[${blocks}]`;
+}
+
+function submitIndexInput(e) {
+  e.preventDefault();
+  /* displayInput.innerText = `[${arrayBlockInput.value}]`; */
+  let indexStart = parseInt(startingBlockInput.value);
+  displayStaringIndexInput.innerText = `Frogs will be initially placed in Block Index [${indexStart}]`;
+  console.log(indexStart);
+  console.log(blocks);
+  solution(blocks, indexStart);
+}
+submit.addEventListener("click", submitInput);
+submitIndex.addEventListener("click", submitIndexInput);
+console.log(blocks);
+
+//Solution function
 function solution(blocks, startingBlock) {
   let frogA = 1;
   let frogB = 0;
@@ -31,8 +82,11 @@ function solution(blocks, startingBlock) {
     }
   }
   console.log(
-    `The longest possible distance between the two frogs is ${frogA + frogB}`
+    `The longest possible distance between the two frogs is ${
+      frogA + frogB
+    } blocks`
   );
+  longestDistanceApart.innerText = `The frogs have jumped away from each other starting from block ${indexStart}! The longest possible distance between the two frogs after the jump is ${
+    frogA + frogB
+  } blocks`;
 }
-
-solution([2, 6, 8, 5], 0);
